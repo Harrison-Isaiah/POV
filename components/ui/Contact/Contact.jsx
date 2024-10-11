05e8ce54ec600c7f4c68f75b073cf909662f301a
Harrison Isaiah <97739869+Harrison-Isaiah@users.noreply.github.com>
2024-10-11 18:01:58 +0500

    Add files via upload

---
+++ components/ui/Contact/Contact.jsx
@@ -0,0 +1,124 @@
+
+
+"use client";
+import React, { useState } from "react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogFooter,
+} from "@/components/ui/dialog";
+
+const Contact = () => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    formData.append("access_key", "WATER");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    });
+    const result = await response.json();
+    if (result.success) {
+      console.log(result);
+      setIsPopupOpen(true);
+    }
+  };
+
+  return (
+    <section className="min-h-screen flex items-center justify-center bg-black dark:bg-gray-900">
+      <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-black dark:bg-black">
+      <h2 className="font-bold text-xl text-white dark:text-neutral-200">
+        Let's Connect
+        </h2>
+        <p className="text-gray-200 text-sm max-w-sm mt-2 dark:text-neutral-300">
+        "Get in touch with us to bring your ideas to life—whether it's web development, mobile apps, or custom software solutions, we're here to help you succeed!"
+        </p>
+        <form className="my-8" onSubmit={handleSubmit}>
+          <div  className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+            <LabelInputContainer>
+              <Label htmlFor="firstname">First name</Label>
+              <Input id="firstname" placeholder="Tyler" type="text" name="First_Name" />
+            </LabelInputContainer>
+            <LabelInputContainer>
+              <Label htmlFor="lastname">Last name</Label>
+              <Input id="lastname" placeholder="Durden" type="text" name="Last_Name" />
+            </LabelInputContainer>
+          </div>
+          <LabelInputContainer className="mb-4">
+            <Label htmlFor="email">Email Address</Label>
+            <Input id="email" placeholder="projectmayhem@fc.com" type="email" name="Email" />
+          </LabelInputContainer>
+          <LabelInputContainer className="mb-4">
+            <Label htmlFor="message">Message</Label>
+            <textarea
+              className="flex min-h-[2.5rem] w-full border-none bg-zinc-800 dark:bg-zinc-800 text-white dark:text-white shadow-input rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)]"
+              name="message"
+              rows={3}
+              id="message"
+            />
+          </LabelInputContainer>
+
+          <Button
+             className="bg-zinc-900 dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+             type="submit"
+           >
+            Send &rarr;
+            <BottomGradient />
+          </Button>
+        </form>
+      </div>
+
+      <Dialog open={isPopupOpen} onOpenChange={setIsPopupOpen}>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Message Sent</DialogTitle>
+            <DialogDescription>
+              Your message has been successfully sent. We'll get back to you soon!
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <Button onClick={() => setIsPopupOpen(false)}>OK</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </section>
+  );
+}
+
+const BottomGradient = () => {
+  return (
+    <>
+      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
+      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-cyan-800 to-transparent" />
+    </>
+  );
+};
+
+const LabelInputContainer = ({ children, className }) => {
+  return (
+    <div className={cn("flex flex-col space-y-2 w-full", className)}>
+      {children}
+    </div>
+  );
+};
+
+export default Contact;
